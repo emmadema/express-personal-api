a5f4808d56174659ebab7c93108c4be15e538700
emmadema <emma.demango@gmail.com>
2017-10-11 16:53:28 -0600

    not working

--- server.js
+++ server.js
@@ -18,15 +18,17 @@ mongooe.connect("mongod://localhost/hikes")
 
 // var db = require('./models');
 
-/**********
- * ROUTES *
- **********/
 var hikes =[
 {"id": 0, "name": "Mount Audubon Trail", "diffiuclty": "Hard", "length": "7.6 miles", "elevationGain": "2,667 ft", "location": "Indian Peaks Wilderness Area, Ward, CO"},
 {"id": 1, "name": "Mowhawk Lakes", "diffiuclty": "Moderate", "length": "8.9 miles", "elevationGain": "2,185 ft", "location": "Breckenridge, CO"},
 {"id": 2, "name": "Sky Pond", "diffiuclty": "Hard", "length": "8.4 miles", "elevationGain": "1,833 ft", "location": "Rocky Mountain National Park, Estes Park, CO"},
 {"id": 3, "name": "South Bolder Peak Trail", "diffiuclty": "Hard", "length": "7.8 miles", "elevationGain": "3,090 ft", "location": "Boulder, CO"}
 ];
+
+/**********
+ * ROUTES *
+ **********/
+
 
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
@@ -51,12 +53,12 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/emmadema/express-personal-api.git", // CHANGE ME
+    base_url: "http://stark-coast-26914.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/hikes", description: "E.g. log a new hike"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "about me"}, // CHANGE ME
+      {method: "POST", path: "/api/hikes", description: "log a new hike"} // CHANGE ME
     ]
   });
 });
