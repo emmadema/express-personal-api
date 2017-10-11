fde9af61fc6620b54d399257c0537d13d03b2003
emmadema <emma.demango@gmail.com>
2017-10-11 16:47:05 -0600

    testing array

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const mongoose = require('mongoose');
+
+mongooe.connect("mongod://localhost/hikes")
+
 /************
  * DATABASE *
  ************/
@@ -17,6 +21,12 @@ app.use(bodyParser.json());
 /**********
  * ROUTES *
  **********/
+var hikes =[
+{"id": 0, "name": "Mount Audubon Trail", "diffiuclty": "Hard", "length": "7.6 miles", "elevationGain": "2,667 ft", "location": "Indian Peaks Wilderness Area, Ward, CO"},
+{"id": 1, "name": "Mowhawk Lakes", "diffiuclty": "Moderate", "length": "8.9 miles", "elevationGain": "2,185 ft", "location": "Breckenridge, CO"},
+{"id": 2, "name": "Sky Pond", "diffiuclty": "Hard", "length": "8.4 miles", "elevationGain": "1,833 ft", "location": "Rocky Mountain National Park, Estes Park, CO"},
+{"id": 3, "name": "South Bolder Peak Trail", "diffiuclty": "Hard", "length": "7.8 miles", "elevationGain": "3,090 ft", "location": "Boulder, CO"}
+];
 
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
@@ -28,6 +38,7 @@ app.use(express.static('public'));
 
 app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+  res.json(hikes);
 });
 
 
@@ -45,9 +56,9 @@ app.get('/api', function api_index(req, res) {
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/hikes", description: "E.g. log a new hike"} // CHANGE ME
     ]
-  })
+  });
 });
 
 /**********
